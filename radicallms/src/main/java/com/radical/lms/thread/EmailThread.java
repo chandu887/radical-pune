3e20dd89b65876cd8d6d70260d0b975ac4bd5927
chandu887 <chandrasekhar.mutta8@gmail.com>
2016-11-25 08:41:48 +0530

    committed missing filling file

---
+++ radicallms/src/main/java/com/radical/lms/thread/EmailThread.java
@@ -0,0 +1,50 @@
+package com.radical.lms.thread;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.radical.lms.entity.CourseEntity;
+import com.radical.lms.entity.SendEmailEntity;
+import com.radical.lms.service.EmailService;
+import com.radical.lms.service.UserService;
+
+public class EmailThread implements Runnable {
+
+	
+	public void run() {  
+		System.out.println("Emailthread calling");
+		try {
+			Thread.currentThread().sleep(300000);
+			ApplicationContext springContext = WebApplicationContextUtils.getWebApplicationContext(ContextLoaderListener.getCurrentWebApplicationContext().getServletContext());
+			System.out.println("Emailthread executing");
+			EmailService emailService = (EmailService) springContext.getBean("emailService");
+			UserService userService = (UserService) springContext.getBean("userService");
+
+			emailService.readMailInbox();
+			
+			List<SendEmailEntity> emailEntries = emailService.getEmailEntries();
+			
+			for (SendEmailEntity sendEmailEntity : emailEntries) {
+				CourseEntity courseEntity = userService.getCourseListBasedOnCourseId(sendEmailEntity.getCourseId());
+				
+				boolean emailStatus = emailService.sendMail(sendEmailEntity.getReceiverMailId(), courseEntity.getSubject(), courseEntity.getMessagebody());
+				if (emailStatus) {
+					sendEmailEntity.setStatus(1);
+				} else {
+					sendEmailEntity.setStatus(2);
+				}
+				sendEmailEntity.setEmailSendTime(new Date());
+				emailService.updateEmailEntries(sendEmailEntity);
+			}
+			new Thread(new EmailThread()).start();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+	}  
+	
+}
